41c8a551b1350566ae5958f2fb0b4ee9d3f8d382
kalen510 <kalen.wiley@gmail.com>
2023-04-18 20:15:20 -0400

    edit task date

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,8 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+
+
 // click event for editing task description
 $(".list-group").on("click", "p", function() {
   var text = $(this)
@@ -90,6 +92,59 @@ $(".list-group").on("blur", "textarea", function() {
   // replace textarea with p element
   $(this).replaceWith(taskP);
 });
+
+// click event for editing task date
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// click event for saving edited task date
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update the task in array and re-save to localStorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap class
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
+
+
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
